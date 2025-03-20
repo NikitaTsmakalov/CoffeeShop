14bdc2099df6733c9a46e9acee1a7e46e87918f8
Nikita <nikita4343tg@gmail.com>
2025-03-20 23:22:01 +0300

    firs: banner not working

---
+++ app/src/main/java/com/example/coffeeshop/Activity/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.coffeeshop.Activity
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.coffeeshop.MainActivity
+import com.example.coffeeshop.R
+import com.example.coffeeshop.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener{
+startActivity(Intent(this,MainActivity::class.java))
+        }
+
+    }
+}
